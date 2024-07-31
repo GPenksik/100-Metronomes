f1752ffa6a10c6db3e9e58d53776d886d16e0e85
ztx <ztx@example.com>
2024-08-01 01:12:53 +0800

    set speed function change

--- Assets/scripts/AnimationController.cs
+++ Assets/scripts/AnimationController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class AnimationController : MonoBehaviour
@@ -8,6 +9,10 @@ public class AnimationController : MonoBehaviour
 
     private Animator animator;
     public bool IsReversed = false;
+    private float originalDuration = 0.5f; //原始时长（秒）
+    private float targetDuration; // 目标播放时长
+    private float Alpha = 1.0f;
+    private float time_error = 0f;
     void Start()
     {
         animator = GetComponent<Animator>();
@@ -20,8 +25,37 @@ public class AnimationController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ChangeAnimationSpeed();
     }
+
+    void ChangeAnimationSpeed()
+    {
+        // 检测空格键按下
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // 记录空格键按下的时间戳
+            TimeManager.Instance.AddTimestamp(Time.time);
+            if (TimeManager.Instance.GetsatampNum() > 2)
+            {
+                targetDuration = TimeManager.Instance.GetTimeDifferenceBetweenLastTwoTimestamps();
+                time_error = targetDuration - originalDuration;
+                //time_error = targetDuration - originalDuration + time_error;
+                //targetDuration = time_error * Alpha + originalDuration;
+                Debug.Log("target Duration: " + targetDuration);
+                UpdateAnimationSpeed();
+            }
+        }
+    }
+
+    void UpdateAnimationSpeed()
+    {
+        SetAnimationSpeedsign();
+        float newSpeed = originalDuration / targetDuration;
+        //Debug.Log("target Duration: " + targetDuration);
+        SetAnimationSpeed(newSpeed);
+
+    }
+
 
     public void SetAnimationSpeed(float speed)
     {
@@ -36,7 +70,7 @@ public class AnimationController : MonoBehaviour
     {
         //return animator != null ? animator.speed : 0f;
         if (animator != null)
-        {
+        { 
             return animator.speed >= 0 ? 1 : -1;
         }
         else
--- Assets/scripts/Asynchrony.cs
+++ Assets/scripts/Asynchrony.cs
@@ -7,12 +7,6 @@ using UnityEngine;
 public class Asynchrony : MonoBehaviour
 {
 
-    public AnimationController Shaft1; // 引用 Shaft1 的脚本
-    private float originalDuration = 0.5f; //原始时长（秒）
-    private float targetDuration; // 目标播放时长
-    private float Alpha = 1.0f;
-    private float time_error = 0;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -22,32 +16,6 @@ public class Asynchrony : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Synchronize();
-
-    }
-    void Synchronize()
-    {
-        // 检测空格键按下
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            // 记录空格键按下的时间戳
-            TimeManager.Instance.AddTimestamp(Time.time);
-            if (TimeManager.Instance.GetsatampNum() > 5)
-            {
-                targetDuration = TimeManager.Instance.GetTimeDifferenceBetweenLastTwoTimestamps();
-                time_error = targetDuration - originalDuration + time_error;
-                targetDuration = time_error * Alpha + originalDuration;
-                Debug.Log("target Duration: " + targetDuration);
-                UpdateAnimationSpeed();
-            }
-        }
-    }
-
-    void UpdateAnimationSpeed()
-    {
-        float newSpeed = originalDuration / targetDuration;
-        //Debug.Log("target Duration: " + targetDuration);
-        Shaft1.SetAnimationSpeed(newSpeed);
 
     }
 }
